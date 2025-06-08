9e56213fd966e0339344f910e1cdadbfd147468c
Gabriel Francisco Romero Rocha <109051710+GabRom98@users.noreply.github.com>
2025-06-08 02:12:01 -0300

    Se anadio cors

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import mainRouter from "./src/routes/mainRouter.js";
 import morgan from "morgan";
+import cors from 'cors';
 
 const server = express();
 
 const HOSTNAME = '127.0.0.1';
 const PORT = process.env.PORT || 8080;
+
+const corsOptions = {
+  origin: '*',  
+  methods: 'GET,POST,PUT,DELETE',  
+  allowedHeaders: 'Content-Type,Authorization',  
+  credentials: true,
+};
+
+server.use(cors(corsOptions));
 
 server.use(express.json())
 server.use(express.text())
